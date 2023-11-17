0698ef223d64be772bdf73d308dfb17af5d09036
S4nt1ag0 <gustavosantiago1018@gmail.com>
2023-11-17 10:53:06 -0300

    add dotenv and api_key

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 app.get('/', function(req,res) {
     res.send('Hello Word')
@@ -7,4 +8,5 @@ app.get('/', function(req,res) {
 
 app.listen(3000,function(){
     console.log('Examplo app escutando requisições na porta 3000')
+    console.log(process.env.API_KEY)
 })
