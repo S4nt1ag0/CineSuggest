2ee9d850ca6faa1949a96729927025810c758d05
S4nt1ag0 <gustavosantiago1018@gmail.com>
2023-11-17 15:47:54 -0300

    add routes of list movies and details

---
+++ backend/src/controllers/movies.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+module.exports = {
+    discovery: async function(req, res, next){
+        const response = await axios.get('/discover/movie', {
+            params: { 
+                api_key: process.env.API_KEY,
+                sort_by: req.query.sortBy || 'popularity.desc',
+                page: req.query.page || 1,
+                language: req.query.language || 'pt-BR'
+            }
+        });
+    
+    res.send(response.data)
+    },
+ 
+    details: async function(req, res, next){
+        if(req.query.ID){
+            const response = await axios.get(`/movie/${req.query.ID}`, {
+                params: { 
+                    api_key: process.env.API_KEY,
+                    language: req.query.language || 'pt-BR'
+                }
+        });
+        res.send(response.data)
+        }else{
+            res.send("ID não informado ou incorreto")
+        }
+
+        
+    }
+}
---
+++ backend/src/routers/movies.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router();
+
+const moviesController = require('../controllers/movies');
+
+router.get('/', moviesController.discovery)
+
+router.get('/details', moviesController.details)
+
+module.exports = router;
