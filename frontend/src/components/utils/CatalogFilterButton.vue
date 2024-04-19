d1bec09e7f7a04aadb801ee1a5e08343354eca62
S4nt1ag0 <gustavosantiago1018@gmail.com>
2024-04-18 22:16:25 -0300

    catalog-component-for-desktop

--- frontend/src/components/utils/CatalogFilterButton.vue
+++ frontend/src/components/utils/CatalogFilterButton.vue
@@ -1,22 +1,39 @@
 <template>
-  <button @click="dropped = !dropped" class="catalogButton">
-    <CatalogIcon width="32px" height="32px" />
-    <p>Catálogo</p>
-    <ArrowRightIcon v-if="dropped" width="20px" height="20px" class="catalogArrows" />
-    <ArrowDownIcon v-else width="20px" height="20px" class="catalogArrows" />
-  </button>
+  <div class="catalog_filter_container">
+    <button @click="dropped = !dropped" class="catalogButton">
+      <CatalogIcon width="32px" height="32px" />
+      <p>Catálogo</p>
+      <ArrowRightIcon v-if="dropped" width="20px" height="20px" class="catalogArrows" />
+      <ArrowDownIcon v-else width="20px" height="20px" class="catalogArrows" />
+    </button>
+    <div class="catalog_filter_content" v-if="dropped">
+      <ButtonComponent>Ação</ButtonComponent>
+      <ButtonComponent>Aventura</ButtonComponent>
+      <ButtonComponent>Terror</ButtonComponent>
+      <ButtonComponent>Misterio</ButtonComponent>
+      <ButtonComponent>Comédia</ButtonComponent>
+      <ButtonComponent>Drama</ButtonComponent>
+      <ButtonComponent>Ficção Cientifica</ButtonComponent>
+      <ButtonComponent>Fantasia</ButtonComponent>
+      <ButtonComponent>Suspense</ButtonComponent>
+      <ButtonComponent>Sobrenatural</ButtonComponent>
+      <ButtonComponent>Esportes</ButtonComponent>
+    </div>
+  </div>
 </template>
 
 <script>
 import ArrowRightIcon from '../icons/ArrowRightIcon.vue'
 import ArrowDownIcon from '../icons/ArrowDownIcon.vue'
 import CatalogIcon from '../icons/catalogIcon.vue'
+import ButtonComponent from './ButtonComponent.vue'
 
 export default {
   components: {
     CatalogIcon,
     ArrowRightIcon,
-    ArrowDownIcon
+    ArrowDownIcon,
+    ButtonComponent
   },
   data() {
     return {
@@ -26,35 +43,70 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-.catalogButton {
-  background-color: transparent;
-  border: none;
-  font-size: 1rem;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0.5rem 1rem;
-  transition: 0.4s;
-  font-weight: bold;
-  color: var(--color-text);
+.catalog_filter_container {
+  position: relative;
+  .catalogButton {
+    background-color: transparent;
+    border: none;
+    font-size: 1rem;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.5rem 1rem;
+    transition: 0.4s;
+    font-weight: bold;
+    color: var(--color-text);
+  }
+  .catalog_filter_content {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    top: 50px;
+    background-color: #343a3f;
+    min-width: 200px;
+    border-radius: 10px;
+    max-height: 220px;
+    flex-wrap: wrap;
+    width: max-content;
+    button {
+      border-right-style: solid;
+      border-bottom-style: solid;
+      border-width: 1px;
+      border-color: #c0c0c0;
+      width: 200px;
+      color: #c0c0c0;
+    }
+    button:hover {
+      cursor: pointer;
+      background-color: #6d7175;
+    }
+  }
 }
 
 @media (max-width: 1024px) {
-  .catalogButton {
-    padding: 0.5rem 0.75rem;
+  .catalog_filter_container {
+    .catalogButton {
+      padding: 0.5rem 0.75rem;
+    }
   }
 }
 @media (max-width: 768px) {
-  .catalogButton {
-    display: flex;
-    flex-direction: column;
+  .catalog_filter_container {
+    .catalogButton {
+      display: flex;
+      flex-direction: column;
 
-    p {
-      margin-top: 5px;
+      p {
+        margin-top: 5px;
+      }
+      .catalogArrows {
+        display: none;
+      }
     }
-    .catalogArrows {
-      display: none;
+    .catalog_filter_content {
+      bottom: 0;
     }
   }
 }
